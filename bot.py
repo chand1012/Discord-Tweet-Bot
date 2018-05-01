41878e05824824a3621523572c898dcc99950ecf
chand1012 <chandlerl14@outlook.com>
2018-05-01 16:29:57 +0000

    Added a lot of things

--- bot.py
+++ bot.py
@@ -1,16 +1,17 @@
 import tweepy
-#import discord
+import discord
 from lib import *
 from random import choice
 from json import dumps
 
 keys = get_keys()
-#people = get_list()
+people = get_list()
 
 tconsumer = keys["consumer_key"]
 tsecret = keys["consumer_secret"]
 taccess = keys["access_token"]
 tsaccess = keys["access_secret"]
+token = keys["discord_token"]
 
 auth = tweepy.OAuthHandler(tconsumer, tsecret)
 auth.set_access_token(taccess, tsaccess)
@@ -20,9 +21,61 @@ def get_tweet(person):
     some_tweets = api.user_timeline(screen_name=person, count=200)
     valid_tweets = []
     for tweet in some_tweets:
-        if (not 'http' in tweet.text) and (not 'RT' in tweet.text):
+        if (not 'http' in tweet.text) and (not 'RT' in tweet.text) and (not '@' in tweet.text):
             valid_tweets += [tweet.text]
             
     return choice(valid_tweets)
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    recv = message.content
+    channel = message.channel
+    if message.author == client.user:
+        pass
+    if recv.startswith("!tweet"):
+        if recv[7:] is '':
+            person = choice(people)
+            tweet = get_tweet(person)
+            msg = "From {}:\n{}".format(person, tweet)
+            print("Posting tweet....")
+            print("Author: {}".format(person))
+            print(tweet)
+            print("------")
+            await client.send_message(channel, content=msg, tts=True)
+        else:
+            msg = None
+            count = 0
+            person = recv[7:]
     
-print(get_tweet('officialjaden'))
+            while True:
+                count+=1
+                tweet = get_tweet(person)
+                if not tweet==None:
+                    msg = "From {}:\n{}".format(person, tweet)
+                    break
+                if count>=10:
+                    print("Cannot find acceptable tweet for user {}".format(person))
+                    print("------")
+                    msg = "Cannot find acceptable tweet. Please try another user."
+                    break
+            await client.send_message(channel, content=msg, tts=True)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+while True:
+    try:
+        client.run(token)
+    except Exception as e:
+        if "Event loop" in str(e):
+            print("\nStopping bot....")
+            break
+        else:
+            print(e)
+            continue
